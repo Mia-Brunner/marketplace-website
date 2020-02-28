6ff4ae71317afc18c397839f6e83476eaad1b03a
MiaBrunner <mbrunner.mktg@gmail.com>
2020-02-28 15:30:04 +1100

    added navbar

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,8 @@
 class ListingsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_listing, only: [:show, :edit, :update, :destroy]
-
+  
 
   def index
     @listings = Listing.all
