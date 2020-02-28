43a696e4e7bc195c17d465a1eec83e45984edd47
MiaBrunner <mbrunner.mktg@gmail.com>
2020-02-28 16:59:35 +1100

    added profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_044402) do
+ActiveRecord::Schema.define(version: 2020_02_28_054041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 2020_02_28_044402) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.text "location"
+    t.integer "contact"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +51,5 @@ ActiveRecord::Schema.define(version: 2020_02_28_044402) do
   end
 
   add_foreign_key "listings", "users"
+  add_foreign_key "profiles", "users"
 end
