e66926b1f53b4c4f55ccf45837cce41127961adc
MiaBrunner <mbrunner.mktg@gmail.com>
2020-03-01 10:41:18 +1100

    added seller info

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :views
   devise_for :users
   # get 'welcome/index'
   # root to: 'welcome#index'
   resources :listings
-
+  resources :users
   root to: 'listings#index'
 
-
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
 end
